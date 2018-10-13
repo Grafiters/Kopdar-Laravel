08162c3fbd4c706d1b2f08938dc1a8851251c8aa
rizkimufti <bayugrafit@gmail.com>
2018-10-13 15:08:34 +0700

    edited scripts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>My Website</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<body>
-    <h1>WELCOME</h1>
-    <p>ini page welcome</p>
-</body>
-</html>
+@extends('layout.layout')
+
+@section('title')
+    Welcome
+@endsection
+
+@section('content')
+    <h1>Welcome</h1>
+    <p>Welcome Page</p>
+@endsection
